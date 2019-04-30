51e9f11bf22e6a5693092c19b81103a9e9ac0d9b
Ben Jefferies <benjefferies@echosoft.uk>
2019-04-30 16:02:19 +0100

    Automatically deploy lambda/dynamo/api gateway via travis

--- backend/serverless/delete.js
+++ backend/serverless/delete.js
@@ -1,48 +1,40 @@
-const uuidv1 = require('uuid/v1');
-var AWS = require('aws-sdk');
-AWS.config.update({ region: 'eu-west-1' });
-
+var AWS = require("aws-sdk");
+AWS.config.update({ region: "eu-west-1" });
 var ddb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
 
 exports.handler = (event, ctx, callback) => {
-    createBlog(event, callback);
+    deleteBlog(event, callback);
 };
 
-const createBlog = (event, callback) => {
+const deleteBlog = (event, callback) => {
 
-    var blog = JSON.parse(event.body);
-
-    var _id = uuidv1();
-    var title = blog.title;
-    var id = title.split(' ').join('-') + '-' + _id.split('-')[0];
-    var description = blog.description;
-    var markdown = blog.markdown;
+    var req = JSON.parse(event.body);
+    var _id = req.id
+    console.log(_id)
 
     var params = {
         TableName: 'blogs-dev',
-        Item: {
-            '_id': { S: _id },
-            'id': { S: id },
-            'title': { S: title },
-            'description': { S: description },
-            'markdown': { S: markdown }
+        Key: {
+            "_id": { S: _id }
         }
     };
 
     var responseBody = {};
 
-    ddb.putItem(params, function (err, data) {
+    console.log("Attempting delete...");
+    ddb.deleteItem(params, function (err, data) {
         if (err) {
-            console.log("Error", err);
+            console.error("Unable to delete item. Error JSON:", JSON.stringify(err, null, 2));
             responseBody = {
                 "status": "FAILED"
             };
         } else {
-            console.log("Success", data);
+            console.log("DeleteItem succeeded:", JSON.stringify(data, null, 2));
             responseBody = {
                 "status": "SUCCESS"
             };
         }
+
         var response = {
             "statusCode": 200,
             "headers": {
@@ -56,4 +48,6 @@ const createBlog = (event, callback) => {
         callback(null, response)
 
     });
+
 }
+
